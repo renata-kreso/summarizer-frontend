570caacd337af32bd1b7d6f65f4ef634da9e09d8
Renata Krešo <renata.kreso@fer.hr>
2021-03-10 20:07:13 +0100

    Update: design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React from 'react';
 import './App.css';
-import 'fontsource-roboto';
+import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
@@ -12,20 +14,32 @@ const darkTheme = createMuiTheme({
   }
 });
 
-let inputText = "";
-let summary = "Generated summary";
-
 const textBoxRows = 20;
 
 function App() {
+  const [inputText, setInputText] = React.useState('');
+  const handleChange = (event) => {
+    setInputText(event.target.value.trim());
+  };
+  const [outputText, setOutputText] = React.useState('');
+
+  const submit = () => {
+    // TODO: get summary from backend
+    setOutputText('Generated summary');
+  }
+
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <Container>
-        <h1>Text summarizer app</h1>
-        <Grid container>
+        <Typography variant="h3" gutterBottom>
+          Text summarizer app
+        </Typography>
+        <Grid container spacing={2}>
           <Grid item xs={6}>
-            <h3>Input text</h3>
+            <Typography variant="h5" gutterBottom>
+              Input text
+            </Typography>
             <TextField
               id="input-text"
               label="Long text"
@@ -34,23 +48,30 @@ function App() {
               variant="outlined"
               fullWidth="true"
               required="true"
+              color="secondary"
               value={inputText}
+              onChange={handleChange}
             />
           </Grid>
           <Grid item xs={6}>
-            <h3>TLDR</h3>
+            <Typography variant="h5" gutterBottom>
+              TLDR
+            </Typography>
             <TextField
               id="output-text"
+              label="Summary"
               multiline
               rows={textBoxRows}
               variant="outlined"
               fullWidth="true"
               disabled="true"
-              value={summary}
+              value={outputText}
             />
           </Grid>
         </Grid>
-
+        <Grid container direction="row" justify="center" alignItems="center" className="btn-submit">
+          <Button disabled={!inputText.trim()} variant="outlined" color="secondary" onClick={submit}>Summarize</Button>
+        </Grid>
       </Container>
     </ThemeProvider>
   );
