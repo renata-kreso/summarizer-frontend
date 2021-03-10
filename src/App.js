55ced559429f9e01778c8d3cf2ef144488dc6008
Renata Krešo <renata.kreso@fer.hr>
2021-03-10 21:25:17 +0100

    Add: copy to clipboard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
@@ -7,6 +6,10 @@ import Button from '@material-ui/core/Button';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
+import Tooltip from '@material-ui/core/Tooltip';
+import AssignmentOutlinedIcon from '@material-ui/icons/AssignmentOutlined';
+
+import './App.css';
 
 const darkTheme = createMuiTheme({
   palette: {
@@ -19,13 +22,17 @@ const textBoxRows = 20;
 function App() {
   const [inputText, setInputText] = React.useState('');
   const handleChange = (event) => {
-    setInputText(event.target.value.trim());
+    setInputText(event.target.value);
   };
   const [outputText, setOutputText] = React.useState('');
 
   const submit = () => {
     // TODO: get summary from backend
     setOutputText('Generated summary');
+  }
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(outputText);
   }
 
   return (
@@ -53,10 +60,19 @@ function App() {
               onChange={handleChange}
             />
           </Grid>
-          <Grid item xs={6}>
-            <Typography variant="h5" gutterBottom>
-              TLDR
+          <Grid container item xs={6} justify="space-between">
+            <Grid item>
+              <Typography variant="h5" gutterBottom>
+                TLDR
             </Typography>
+            </Grid>
+            <Grid item>
+              <Tooltip aria-label="copy" title="Copy to clipboard">
+                <Button onClick={copyToClipboard} >
+                  <AssignmentOutlinedIcon />
+                </Button>
+              </Tooltip>
+            </Grid>
             <TextField
               id="output-text"
               label="Summary"
@@ -70,7 +86,7 @@ function App() {
           </Grid>
         </Grid>
         <Grid container direction="row" justify="center" alignItems="center" className="btn-submit">
-          <Button disabled={!inputText.trim()} variant="outlined" color="secondary" onClick={submit}>Summarize</Button>
+          <Button disabled={!inputText} variant="outlined" color="secondary" onClick={submit}>Summarize</Button>
         </Grid>
       </Container>
     </ThemeProvider>
